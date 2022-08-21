7fe62ea9c70bb7173ab83858909be0dee1bac793
Yuji Teshima <yuji.teshima@gmail.com>
2022-08-21 23:53:48 +0900

    LayoutFile作成

--- src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-    return (
-        <footer className="bg-gray-100">
-            <div className="max-w-4xl w-full mx-auto h-24 flex items-center justify-center">
-                <div>© My Blog</div>
-            </div>
-        </footer>
-    );
-}
