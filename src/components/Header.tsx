f40cb85cafaab117f97489b7d33eec32291ca359
Yuji Teshima <yuji.teshima@gmail.com>
2022-09-06 23:43:09 +0900

    main コンテンツのwidthを変更

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,9 +3,9 @@ import Link from 'next/link';
 const Header = () => {
   return (
     <header className="sticky top-0 border-b z-10 bg-white">
-      <div className="max-w-4xl mx-auto flex justify-between items-center h-12">
+      <div className="max-w-screen-xl mx-auto flex justify-between items-center h-12">
         <Link href="/">
-          <a>SiteName</a>
+          <a>ABlog</a>
         </Link>
         {/* <div>Link</div> */}
         <div>
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Footer from './Footer';
 import Header from './Header';
+import Header2 from './Header2';
 
 type LayoutProps = Required<{
   readonly children?: React.ReactNode;
@@ -10,7 +11,7 @@ const Layout = ({ children }: LayoutProps) => {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
-      <main className="flex-1 max-w-4xl w-full mx-auto">{children}</main>
+      <main className="flex-1 max-w-screen-xl w-full mx-auto">{children}</main>
       <Footer />
     </div>
   );
