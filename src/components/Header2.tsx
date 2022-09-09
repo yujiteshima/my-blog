8c0f4858ded7dc08e5ef3c471a1f0ab4701abe14
Yuji Teshima <yuji.teshima@gmail.com>
2022-09-09 22:46:20 +0900

    add Header2.tsx

---
+++ src/components/Header2.tsx
@@ -0,0 +1,21 @@
+import Link from 'next/link';
+
+const Header = () => {
+    return (
+        <header className="sticky top-0 border-b z-10 bg-white">
+            <div className="max-w-screen-xl mx-auto flex justify-center items-center h-12">
+                <Link href="/">
+                    <a>ABlog</a>
+                </Link>
+                {/* <div>Link</div> */}
+                <div>
+                    <Link href="/">
+                        <a>AboutMe</a>
+                    </Link>
+                </div>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
